a2677087c91e7cd1d8b468b0406f5f6c7c647650
kkh2415 <kkh2415@naver.com>
2022-04-09 20:43:10 +0900

    feature : post service 삭제, 조회 로직 추가

--- src/main/java/com/together/community/service/PostService.java
+++ src/main/java/com/together/community/service/PostService.java
@@ -1,6 +1,5 @@
 package com.together.community.service;
 
-import com.together.community.controller.dto.PostDto;
 import com.together.community.domain.Post;
 import com.together.community.domain.member.Member;
 import com.together.community.repository.member.MemberRepository;
@@ -9,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class PostService {
 
@@ -20,7 +21,6 @@ public class PostService {
     /**
      * 게시글 등록
      * */
-    @Transactional
     public Long posting(Long memberId, String title, String content) {
         Member member = memberRepository.findById(memberId);
         Post post = Post.createPost(member, title, content);
@@ -31,18 +31,33 @@ public class PostService {
     /**
      * 게시글 수정
      * */
-    @Transactional
-    public void updatePost(Long postId, PostDto postDto) {
-        Post post = postRepository.findById(postId);
-        post.setTitle(postDto.getTitle());
-        post.setContent(postDto.getContent());
-
+    public void updatePost(Long postId, String title, String content) {
         /**
          * @Transactional에 의해서 commit이 진행됨
          * 커밋이 딱 되면 jpa가 플러쉬를 날린다.
          * 그러면 엔티티 중에서 변경감지가 일어난 애를 다 찾는다.
          * 그러면 UPDATE 쿼리를 바뀐 엔티티에 맞게 DB에 알아서 날려준다
          * */
+        Post post = postRepository.findById(postId);
+        post.changePost(title, content);
+    }
+
+    /**
+     * 게시글 삭제
+     * */
+    public void deletePost(Long postId) {
+        postRepository.delete(postId);
+    }
+
+    /**
+     * 게시글 조회
+     * */
+    public Post findOne(Long postId) {
+        return postRepository.findById(postId);
+    }
+
+    public List<Post> findBymemberId(Long memberId) {
+        return postRepository.findBymemberId(memberId);
     }
 
 }
--- src/test/java/com/together/community/domain/MemberTest.java
+++ src/test/java/com/together/community/domain/MemberTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -94,6 +95,7 @@ public class MemberTest {
     }
 
     @Test
+    @Commit
     public void commentTest() {
         Member member1 = new Member();
         member1.setLoginId("test0");
