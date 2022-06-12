ee94466d0cc2cf6e30eea65653227ebd0be0e0f9
kkh2415 <kkh2415@naver.com>
2022-06-12 12:29:14 +0900

    refact : backend 서버 url 수정

--- frontend/src/api/backEndHost.js
+++ frontend/src/api/backEndHost.js
@@ -2,6 +2,7 @@ let backendHostUrl;
 
 const hostname = window && window.location && window.location.hostname;
 
+backendHostUrl = "http://3.34.98.169:8080"
 if (hostname === "localhost") {
 	backendHostUrl = "http://localhost:8080"
 }
