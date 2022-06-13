55f2ffd66add54c58cac558f1e22caa4f2319428
kkh2415 <kkh2415@naver.com>
2022-06-13 17:54:46 +0900

    refact : aws 인스턴스 퍼블릭 아이피 변경에 따른 설정정보 변경

--- frontend/src/api/backEndHost.js
+++ frontend/src/api/backEndHost.js
@@ -2,7 +2,7 @@ let backendHostUrl;
 
 const hostname = window && window.location && window.location.hostname;
 
-backendHostUrl = "http://3.34.98.169:8080"
+backendHostUrl = "http://15.164.220.179:8080"
 if (hostname === "localhost") {
 	backendHostUrl = "http://localhost:8080"
 }
