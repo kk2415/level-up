a501e1aceba63c296a1b12d5d111d6602ef67fe3
Kim kyunghee <kkh2415@naver.com>
2022-07-17 00:08:11 +0900

    add : 캐시 설정 파일 추가
    redis에 직렬화해서 보내는 데이터를 알아보기 쉽게 바꾸기 위해 직렬화 구현체를 변경함

---
+++ backend/api/src/main/java/com/levelup/api/config/CacheConfig.java
@@ -0,0 +1,24 @@
+package com.levelup.api.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+    }
+
+}
