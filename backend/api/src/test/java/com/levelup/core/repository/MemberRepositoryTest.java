0367833c05a7705209c619259fc678c8ec72c253
Kim kyunghee <kkh2415@naver.com>
2022-07-31 00:18:41 +0900

    add : #94 - 멤버 레포지토리 테스트케이스 추가

--- backend/api/src/test/java/com/levelup/core/repository/MemberRepositoryTest.java
+++ backend/api/src/test/java/com/levelup/core/repository/MemberRepositoryTest.java
@@ -2,7 +2,10 @@ package com.levelup.core.repository;
 
 import com.levelup.TestSupporter;
 import com.levelup.api.ApiApplication;
+import com.levelup.core.domain.channel.Channel;
+import com.levelup.core.domain.channel.ChannelMember;
 import com.levelup.core.domain.member.Member;
+import com.levelup.core.repository.channel.ChannelRepository;
 import com.levelup.core.repository.member.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,9 +25,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MemberRepositoryTest extends TestSupporter {
 
     private final MemberRepository memberRepository;
+    private final ChannelRepository channelRepository;
 
-    public MemberRepositoryTest(@Autowired MemberRepository memberRepository) {
+    public MemberRepositoryTest(@Autowired MemberRepository memberRepository,
+                                @Autowired ChannelRepository channelRepository) {
         this.memberRepository = memberRepository;
+        this.channelRepository = channelRepository;
     }
 
     @DisplayName("멤버 생성 및 조회 테스트")
@@ -54,6 +62,32 @@ public class MemberRepositoryTest extends TestSupporter {
 
         // Then
         assertThat(findMember).isNull();
+    }
+
+    @DisplayName("채널 가입된 멤버 조회 테스트")
+    @Test
+    void findByChannelId() {
+        // Given
+        Member manager = createMember("testEmail@test.com", "testUser");
+        Member member1 = createMember("testEmail@test.com", "testUser");
+        Member member2 = createMember("testEmail@test.com", "testUser");
+        memberRepository.save(manager);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Channel channel = createChannel(manager, "testChannel");
+        channelRepository.save(channel);
+
+        ChannelMember channelMember1 = ChannelMember.createChannelMember(member1, false, false);
+        ChannelMember channelMember2 = ChannelMember.createChannelMember(member2, false, false);
+        channel.setChannelMember(channelMember1);
+        channel.setChannelMember(channelMember2);
+
+        // When
+        List<Member> findMembers = memberRepository.findByChannelId(channel.getId());
+
+        // Then
+        assertThat(findMembers.size()).isEqualTo(3);
     }
 
 }
