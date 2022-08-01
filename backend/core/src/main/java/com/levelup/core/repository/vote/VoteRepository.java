0926f9d47c045c55fd5d8d06870e0ef45ef1beff
Kim kyunghee <kkh2415@naver.com>
2022-08-01 15:19:36 +0900

    fix : @Query 애노테이션이 아니라 data jpa 네이밍 전략으로 변경

--- backend/core/src/main/java/com/levelup/core/repository/vote/VoteRepository.java
+++ backend/core/src/main/java/com/levelup/core/repository/vote/VoteRepository.java
@@ -4,15 +4,11 @@ package com.levelup.core.repository.vote;
 import com.levelup.core.domain.vote.Vote;
 import com.levelup.core.domain.vote.VoteType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
-    @Query("select v from Vote v " +
-                    "where v.memberId = :memberId and v.targetId = :targetId and v.voteType = :voteType")
-    Optional<List<Vote>> findByMemberAndTargetAndVoteType(Long memberId, Long targetId, VoteType voteType);
+    List<Vote> findByMemberIdAndTargetIdAndVoteType(Long memberId, Long targetId, VoteType voteType);
 }
