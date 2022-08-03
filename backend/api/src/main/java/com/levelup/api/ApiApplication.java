3221eba263b8aa2bd357e4e1918fda7d90c6a177
Kim kyunghee <kkh2415@naver.com>
2022-08-03 11:08:08 +0900

    fix : 기존 ApiApplication 클래스에 있던 jpa 설정들을 JpaConfiguration으로 분리

--- backend/api/src/main/java/com/levelup/api/ApiApplication.java
+++ backend/api/src/main/java/com/levelup/api/ApiApplication.java
@@ -9,14 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableGlobalMethodSecurity(securedEnabled = true) //secured 애노테이션 활성화
 @EnableCaching
-@EntityScan("com.levelup.core")
-@EnableJpaRepositories("com.levelup.core")
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @SpringBootApplication(scanBasePackages = "com.levelup")
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-
 }
