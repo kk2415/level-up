3c97697e537e6a62d8d89974c638ab9b4510b3db
Kim kyunghee <kkh2415@naver.com>
2022-08-23 12:35:31 +0900

    add : ChannelPostRequest DTO에 펙토리 메서드 추가

--- backend/core/src/main/java/com/levelup/core/dto/channelPost/ChannelPostRequest.java
+++ backend/core/src/main/java/com/levelup/core/dto/channelPost/ChannelPostRequest.java
@@ -5,7 +5,6 @@ import com.levelup.core.domain.channelPost.ChannelPost;
 import com.levelup.core.domain.channel.Channel;
 import com.levelup.core.domain.member.Member;
 import com.levelup.core.domain.channelPost.PostCategory;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,13 +12,23 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class ChannelPostRequest {
 
     private String title;
     private String content;
     private ArticleType articleType;
     private PostCategory postCategory;
+
+    private ChannelPostRequest(String title, String content, ArticleType articleType, PostCategory postCategory) {
+        this.title = title;
+        this.content = content;
+        this.articleType = articleType;
+        this.postCategory = postCategory;
+    }
+
+    public static ChannelPostRequest of(String title, String content, ArticleType articleType, PostCategory postCategory) {
+        return new ChannelPostRequest(title, content, articleType, postCategory);
+    }
 
     public ChannelPost toEntity(Member member, Channel channel) {
         ChannelPost channelPost = ChannelPost.builder()
