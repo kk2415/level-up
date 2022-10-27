214146c8c0ad6b4944acaf5d4cfa891c3420b5c2
Kim kyunghee <kkh2415@naver.com>
2022-10-27 17:36:23 +0900

    add : 기본 이미지 URL 추가

--- frontend/src/api/const/BackEndHost.js
+++ frontend/src/api/const/BackEndHost.js
@@ -9,5 +9,6 @@ if (hostname === "localhost") {
 }
 
 export const S3_URL = "https://mylevelupbuckets3.s3.ap-northeast-2.amazonaws.com/"
+export const DEFAULT_IMAGE_URL = 'thumbnail/fb23d674-c0ed-417c-b732-6743b8989406.png'
 export const SERVICE_APP_URL = `${backendHostUrl}`
 export const IMAGE_SERVER_URL = `${imageServerUrl}`
