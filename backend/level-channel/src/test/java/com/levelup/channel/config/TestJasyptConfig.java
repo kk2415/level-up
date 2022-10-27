7a59e7ca43b5a7bd35b9537b7b2ca4f5d70233c5
Kim kyunghee <kkh2415@naver.com>
2022-10-27 16:04:55 +0900

    add : jasypt 라이브러리를 이용한 개인정보 암호화

---
+++ backend/level-channel/src/test/java/com/levelup/channel/config/TestJasyptConfig.java
@@ -0,0 +1,35 @@
+package com.levelup.channel.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestJasyptConfig {
+
+//    private final String password;
+//
+//    public JasyptConfig(@Value("${jasypt.encryptor.password}") String password) {
+//        this.password = password;
+//    }
+
+    @Bean("testEncryptorBean")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword("p3uVnuqs1asQda");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
