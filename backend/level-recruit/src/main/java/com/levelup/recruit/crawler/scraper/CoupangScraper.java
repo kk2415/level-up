2651de987b1583351887cdbc807a11c0bde54f0b
Kim kyunghee <kkh2415@naver.com>
2023-02-13 00:03:34 +0900

    fix: 쿠팡 채용 크롤링 라이브러리 selenium으로 변경

--- backend/level-recruit/src/main/java/com/levelup/recruit/crawler/scraper/CoupangScraper.java
+++ backend/level-recruit/src/main/java/com/levelup/recruit/crawler/scraper/CoupangScraper.java
@@ -3,8 +3,14 @@ package com.levelup.recruit.crawler.scraper;
 import com.levelup.recruit.crawler.connetion.JsoupConnectionMaker;
 import com.levelup.recruit.domain.domain.CoupangJob;
 import com.levelup.recruit.domain.domain.Job;
+import com.levelup.recruit.domain.enumeration.Company;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -14,6 +20,9 @@ import java.util.stream.Collectors;
 @Component
 public class CoupangScraper {
 
+    @Value("${webdriver.chrome.driver}")
+    private String chromeDriver;
+
     private final JsoupTemplate jsoupTemplate;
 
     public CoupangScraper(@Qualifier("CoupangConnectionMaker") JsoupConnectionMaker connectionMaker) {
@@ -21,22 +30,26 @@ public class CoupangScraper {
     }
 
     public List<Job> findJobs() {
+        System.setProperty("webdriver.chrome.driver", chromeDriver);
+        WebDriver driver = new ChromeDriver();
+
         int page = 1;
-        int lastPage = 10;
-        String param;
+        int lastPage = 5;
+        String params;
 
         List<Job> jobs = new ArrayList<>();
         for (; page <= lastPage; ++page) {
-            param = "?page=" + page + "&search=Software+Engineer&location=Seoul%2c+South+Korea&pagesize=20#results";
-            Elements jobList = jsoupTemplate.select(param, "div.job-listing > div.card-job");
+            params = "?search=Software+engineer+backend+frontend&location=Seoul%2C+South+Korea&location=South+Korea&pagesize=20#results&page=" + page;
+            driver.get(Company.COUPANG.getUrl() + params);
 
-            List<CoupangJob> scrapedJobs = jobList.stream().map(job -> {
-                final String title = jsoupTemplate.selectSub(job, "div.card-body > h2.card-title > a.stretched-link").text();
-                final String url = "https://www.coupang.jobs" + jsoupTemplate.selectSub(job, "div.card-body > h2.card-title > a.stretched-link").attr("href");
+            List<WebElement> elements = driver.findElements(By.cssSelector("div.job-listing > div.card-job"));
+            List<CoupangJob> scrapedJobs = elements.stream().map((element) -> {
+                final String title = element.findElement(By.cssSelector("div.card-body > h2.card-title > a.stretched-link")).getText();
+                final String url = element.findElement(By.cssSelector("div.card-body > h2.card-title > a.stretched-link")).getAttribute("href");
                 final String noticeEndDate = "채용 마감시";
 
                 return CoupangJob.of(title, url, noticeEndDate);
-            }).collect(Collectors.toUnmodifiableList());
+            }).collect(Collectors.toList());
 
             jobs.addAll(scrapedJobs);
         }
