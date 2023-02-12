44e8f99271eaf0ece51d05b4370e2419c15789e4
Kim kyunghee <kkh2415@naver.com>
2023-02-12 18:14:31 +0900

    fix: 라인 크롤링 라이브러리를 selenium으로 변경

--- backend/level-recruit/src/main/java/com/levelup/recruit/crawler/scraper/LineScraper.java
+++ backend/level-recruit/src/main/java/com/levelup/recruit/crawler/scraper/LineScraper.java
@@ -5,7 +5,12 @@ import com.levelup.recruit.domain.domain.Job;
 import com.levelup.recruit.domain.domain.LineJob;
 import com.levelup.recruit.domain.enumeration.Company;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,6 +19,9 @@ import java.util.stream.Collectors;
 @Component
 public class LineScraper {
 
+    @Value("${webdriver.chrome.driver}")
+    private String chromeDriver;
+
     private JsoupTemplate jsoupTemplate;
 
     public LineScraper(@Qualifier("LineConnectionMaker") JsoupConnectionMaker connectionMaker) {
@@ -21,19 +29,26 @@ public class LineScraper {
     }
 
     public List<Job> findJobs() {
-        String param = "?ca=Engineering&ci=Bundang,Seoul&co=East%20Asia&cp=LINE%20Plus,LINE%20Biz%20Plus,LINE%20Financial%20Plus,LINE%20Studio,LINE%20UP,LINE%20NEXT,LINE%20PLAY,LINE%20Investment%20Technologies%20&fi=Android,Client-side,iOS,Web%20Development,Server-side";
-        Elements jobList = jsoupTemplate.select(param, "ul.job_list > li");
+        System.setProperty("webdriver.chrome.driver", chromeDriver);
+        WebDriver driver = new ChromeDriver();
 
-        return jobList.stream().map(jobElement -> {
-            String title = jsoupTemplate.selectSub(jobElement, "a h3.title").text();
+        String params = "?ca=Engineering&ci=Bundang,Seoul";
+        driver.get(Company.LINE.getUrl() + params);
+        List<WebElement> elements = driver.findElements(By.cssSelector("ul.job_list > li"));
 
-            String jobNoticeUri = jsoupTemplate.selectSub(jobElement, "a").attr("href");
+        List<Job> jobs = elements.stream().map(element -> {
+            String title = element.findElement(By.cssSelector("a h3.title")).getText();
+
+            String jobNoticeUri = element.findElement(By.cssSelector("a")).getAttribute("href");
             String jobNoticePath = jobNoticeUri.substring(jobNoticeUri.lastIndexOf("/"));
             String url = Company.LINE.getUrl() + jobNoticePath;
 
-            String noticeEndDate = jsoupTemplate.selectSub(jobElement, "a span.date").text();
-
+            String noticeEndDate = element.findElement(By.cssSelector("a span.date")).getText();
             return LineJob.of(title, url, noticeEndDate);
-        }).collect(Collectors.toUnmodifiableList());
+        }).collect(Collectors.toList());
+
+        driver.quit();
+
+        return jobs;
     }
 }
