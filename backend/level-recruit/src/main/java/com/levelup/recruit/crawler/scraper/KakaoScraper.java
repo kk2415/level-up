e32f04935f25da5d612f2571c45d14a84c2b831d
Kim kyunghee <kkh2415@naver.com>
2023-02-13 00:27:43 +0900

    fix: 카카오 채용 크롤링 라이브러리 selenium으로 변경

--- backend/level-recruit/src/main/java/com/levelup/recruit/crawler/scraper/KakaoScraper.java
+++ backend/level-recruit/src/main/java/com/levelup/recruit/crawler/scraper/KakaoScraper.java
@@ -4,8 +4,12 @@ import com.levelup.recruit.crawler.connetion.JsoupConnectionMaker;
 import com.levelup.recruit.domain.domain.Job;
 import com.levelup.recruit.domain.domain.KakaoJob;
 import com.levelup.recruit.domain.enumeration.Company;
-import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -14,6 +18,9 @@ import java.util.stream.Collectors;
 
 @Component
 public class KakaoScraper {
+
+    @Value("${webdriver.chrome.driver}")
+    private String chromeDriver;
 
     private final JsoupTemplate jsoupTemplate;
 
@@ -24,19 +31,23 @@ public class KakaoScraper {
     public List<Job> findJobs() {
         int page = 1;
         int lastPage = 5;
-        String param;
+        String params;
+
+        System.setProperty("webdriver.chrome.driver", chromeDriver);
+        WebDriver driver = new ChromeDriver();
 
         List<Job> jobs = new ArrayList<>();
         for (; page <= lastPage; ++page) {
-            param = "?skilset=Android,iOS,Windows,Web_front,DB,Cloud,Server,Hadoop_eco_system,Algorithm_Ranking,System" +
+            params = "?skilset=Android,iOS,Windows,Web_front,DB,Cloud,Server,Hadoop_eco_system,Algorithm_Ranking,System" +
                     "&company=ALL" +
                     "&page=" + page;
-            Elements jobList = jsoupTemplate.select(param, "ul.list_jobs li");
+            driver.get(Company.KAKAO.getUrl() + params);
+            List<WebElement> jobList = driver.findElements(By.cssSelector("ul.list_jobs li"));
 
             List<KakaoJob> scrapedJobs = jobList.stream().map(job -> {
-                final String title = jsoupTemplate.selectSub(job, "a.link_jobs > h4.tit_jobs").text();
-                final String url = Company.KAKAO.getUrl() + jsoupTemplate.selectSub(job, "a.link_jobs").attr("href");
-                final String noticeEndDate = jsoupTemplate.selectSub(job, "dl.list_info > dt:contains(영입마감일) + dd").text();
+                String title = job.findElement(By.cssSelector("a.link_jobs > h4.tit_jobs")).getText();
+                final String url = job.findElement(By.cssSelector("a.link_jobs")).getAttribute("href");
+                final String noticeEndDate = job.findElement(By.cssSelector("dl.list_info > dd")).getText();
 
                 return KakaoJob.of(title, url, noticeEndDate);
             }).collect(Collectors.toUnmodifiableList());
